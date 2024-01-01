7cff76cb76ec5acf716ff932c7ddf0f09fcff125
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-01-01 15:38:52 +0600

    navlinks done

---
+++ components/Logo.tsx
@@ -0,0 +1,22 @@
+import { Instagram } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+function Logo() {
+  return (
+    <Link
+      href={"/dashboard"}
+      className={buttonVariants({
+        className:
+          "navLink !mb-10 hidden md:flex lg:!p-0 lg:hover:bg-transparent",
+        variant: "ghost",
+        size: "lg",
+      })}
+    >
+      <Instagram className="h-6 w-6 shrink-0 lg:hidden" />
+      <p className={`hidden text-xl font-semibold lg:block `}>PixelPeak</p>
+    </Link>
+  );
+}
+
+export default Logo;
--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -1,5 +1,15 @@
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+
 const SideNavbar = () => {
-  return <div>SideNavbar</div>;
+  return (
+    <div className="flex h-full flex-col px-3 md:px-2">
+      <div className="fixed bottom-0 z-50 -ml-3 flex h-16 w-full flex-1 flex-row justify-evenly space-x-2 border-t bg-white p-2 md:relative md:ml-0 md:h-full md:flex-col md:justify-between md:space-x-0 md:space-y-2 md:border-none dark:bg-neutral-950">
+        <Logo />
+        <NavLinks />
+      </div>
+    </div>
+  );
 };
 
 export default SideNavbar;
