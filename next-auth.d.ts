98cfa591914da51691f604e7ed46c46fba6c54f4
Mubasshir Ahmed <lastmubasshir@gmail.com>
2024-01-02 12:25:21 +0600

    prisma and auth added

---
+++ next-auth.d.ts
@@ -0,0 +1,19 @@
+import "next-auth";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    username?: string | null;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      username?: string | null;
+    };
+  }
+  interface User {
+    username?: string | null;
+  }
+}
